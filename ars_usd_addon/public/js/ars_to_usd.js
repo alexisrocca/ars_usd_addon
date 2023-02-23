37eca99f7b6368ecce508ef00de13854a500bd29
alexisrocca <alexis.d.rocca@gmail.com>
2023-02-23 14:23:08 +0000

    Updated

---
+++ ars_usd_addon/public/js/ars_to_usd.js
@@ -0,0 +1,17 @@
+frappe.ui.form.on("Item", {
+    refresh(frm) {
+        
+    }
+})
+frappe.tour['Item'] = [
+	{
+		fieldname: "price_ars",
+		title: "Price ARS",
+		description: __("Price ARS")
+	},
+	{
+		fieldname: "price_usd",
+		title: "Price USD",
+		description: __("Price USD")
+	}
+];
